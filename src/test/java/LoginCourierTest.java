564bef47721b8415e46a9b7f0d2abc30b8803db6
Sergei Trofimov <divisor.null@yandex.com>
2022-10-06 16:50:14 +0300

    1. Первый комит со стартовыми классами

--- src/test/java/LoginCourierTest.java
+++ src/test/java/LoginCourierTest.java
@@ -7,6 +7,8 @@ import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Set;
+
 import static io.restassured.RestAssured.given;
 
 public class LoginCourierTest {
@@ -17,14 +19,12 @@ public class LoginCourierTest {
     @Test
     public void test (){
         LoginCourier login = new LoginCourier("Login23","Password43");
+        Gson gson = new Gson();
         Response response = given()
                 .header("Content-type", "application/json")
                 .and()
                 .body(login)
                 .when()
                 .post("/api/v1/courier/login");
-        System.out.println(response.getBody().asString());
-        //Set<> id = gson.fromJson(response.getBody(),id);
-
     }
 }
